a43734c2cdd8905db9e6da8bdcf9f956499a6a21
Stephan Bartkowiak <stephan@ecobonuz.com>
2019-04-28 12:21:00 -0300

    Creating PORT environment

--- api-start.py
+++ api-start.py
@@ -3,7 +3,9 @@ from flask_restful import Resource, Api
 import src.controller.secret.getsecret as GetSecret
 import src.controller.secret.removeAll as RemoveAll
 import src.controller.secret.remove as Remove
+from decouple import config
 
+PORT = config('PORT', '3000')
 app = Flask(__name__)
 api = Api(app)
 
@@ -12,4 +14,4 @@ api.add_resource(RemoveAll, '/removeall')  # Route 0
 api.add_resource(Remove, '/remove')  # Route 0
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='3000')
+    app.run(host='0.0.0.0', port=PORT)
