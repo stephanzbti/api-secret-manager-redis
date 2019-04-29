99fc21c3b97f64d0596fbf2183c04be9a1b24809
Stephan Bartkowiak <stephan@ecobonuz.com>
2019-04-29 11:06:28 -0300

    Correct Response Metode

--- src/controller/secret/remove.py
+++ src/controller/secret/remove.py
@@ -1,6 +1,6 @@
 from src.dao.aws_connect import get_secret
 from flask_restful import Resource, Api
-from flask import Flask, request
+from flask import Flask, request, Response
 import logging as log
 import json
 from src.dao.dbConn import connectionRedis
@@ -31,11 +31,10 @@ class Remove(Resource):
                 resp = conn.delete(aws_key_word)
                 log.debug('[DEBUG] Remove: Resp: '+json.dumps(resp))
                 log.info('[INFO] RemoveAll: REDIS: Key Deleted!')
-                return {'message': 'REDIS: Key cleaned', 'status': 200}
+                return Response({'message': 'REDIS: Key cleaned'}, status=200, mimetype='application/json')
             else:
                 log.info('[INFO] RemoveAll: REDIS: Key not exists!')
-                return {'message': 'REDIS: Key not exists', 'status': 500}
-
+                return Response({'message': 'REDIS: Key not exists'}, status=500, mimetype='application/json')
         except:
             log.error('[ERROR] Error removing from redis')
-            return {'message': 'Error removing from redis', 'status': 500}
+            return Response({'message': 'Error removing from redis'}, status=500, mimetype='application/json')
